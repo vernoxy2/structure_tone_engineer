6cc1dbd4a669abc66898e0675db7ffd641ac3ff3
vernoxy <vernoxy3@gmail.com>
2025-11-27 09:36:30 +0530

    fix: error solve

--- src/component/GetTouch.jsx
+++ src/component/GetTouch.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Header from '../../../blindschool/src/component/Header'
 
 const GetTouch = ({className = '', className2 = ``}) => {
   return (
