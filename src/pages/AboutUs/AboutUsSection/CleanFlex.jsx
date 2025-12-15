4549083c1b09fe4813c0196750667155d3da399b
vernoxy <vernoxy3@gmail.com>
2025-12-15 16:26:12 +0530

    Fix: Img Errot

--- src/pages/AboutUs/AboutUsSection/CleanFlex.jsx
+++ src/pages/AboutUs/AboutUsSection/CleanFlex.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import image from "../../../assets/AboutUsPageImgs/image.webp";
+import image from "../../../assets/AboutUsPageImgs/AboutUsImg.webp";
 import Title from "../../../component/Title";
 
 const CleanFlex = () => {
